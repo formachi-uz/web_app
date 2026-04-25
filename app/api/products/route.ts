13eae0ff1609bcff50cf941eb8d0a34735a7fcc5
formachi-uz <timonmlbb@gmail.com>
2026-04-26 01:40:52 +0500

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getProducts } from '@/lib/db'
 
+export const dynamic = 'force-dynamic'
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
