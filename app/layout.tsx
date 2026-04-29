e212b00573992f08b7ffa88fe32584c91514bbbc
formachi-uz <timonmlbb@gmail.com>
2026-04-29 17:36:53 +0500

    Load mobile checkout polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import './formachi-fixes.css'
+import './mobile-first.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import CartDrawer from '@/components/CartDrawer'
